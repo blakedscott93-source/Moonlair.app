b86b31d72180d0e8d09badc34f77249722f409a3
blakedscott93-source <blakedscott93@gmail.com>
2025-10-11 21:48:21 -0500

    Added Meta Pixel tracking code for Facebook analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import SchemaMarkup from "@/components/seo/schema-markup";
 import HydrationErrorBoundary from "@/components/ErrorBoundary";
@@ -107,6 +108,31 @@ export default function RootLayout({
             `,
           }}
         />
+        
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1922080955022497');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1922080955022497&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="font-sans antialiased" suppressHydrationWarning={true}>
         <HydrationErrorBoundary />
